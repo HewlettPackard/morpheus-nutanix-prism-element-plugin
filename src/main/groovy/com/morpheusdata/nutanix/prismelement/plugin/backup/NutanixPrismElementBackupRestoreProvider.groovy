22b88bad1bf31dfecf300ac55197e534f2746030
Mike Carlin <mike.carlin@hpe.com>
2024-11-27 10:21:30 -0800

    Add support for backup restore

--- src/main/groovy/com/morpheusdata/nutanix/prismelement/plugin/backup/NutanixPrismElementBackupRestoreProvider.groovy
+++ src/main/groovy/com/morpheusdata/nutanix/prismelement/plugin/backup/NutanixPrismElementBackupRestoreProvider.groovy
@@ -22,12 +22,16 @@ import com.morpheusdata.core.MorpheusContext
 import com.morpheusdata.core.Plugin
 import com.morpheusdata.core.backup.BackupRestoreProvider
 import com.morpheusdata.core.backup.response.BackupRestoreResponse
+import com.morpheusdata.core.util.HttpApiClient
 import com.morpheusdata.model.Backup
 import com.morpheusdata.model.BackupRestore
 import com.morpheusdata.model.BackupResult
 import com.morpheusdata.model.Instance
+import com.morpheusdata.nutanix.prismelement.plugin.utils.NutanixPrismElementApiService
 import com.morpheusdata.response.ServiceResponse
+import groovy.util.logging.Slf4j
 
+@Slf4j
 class NutanixPrismElementBackupRestoreProvider implements BackupRestoreProvider {
 	protected Plugin plugin
 	protected MorpheusContext context
@@ -73,8 +77,51 @@ class NutanixPrismElementBackupRestoreProvider implements BackupRestoreProvider 
 	 * {@inheritDoc}
 	 */
 	@Override
-	ServiceResponse<BackupRestoreResponse> restoreBackup(BackupRestore backupRestoreModel, BackupResult backupResultModel, Backup backupModel, Map opts) {
-		return ServiceResponse.success(new BackupRestoreResponse(backupRestoreModel))
+	ServiceResponse<BackupRestoreResponse> restoreBackup(BackupRestore backupRestore, BackupResult backupResult, Backup backup, Map opts) {
+		log.debug("Restoring backup with result {}", backupResult)
+		ServiceResponse<BackupRestoreResponse> rtn = new ServiceResponse<>(false, null, null, new BackupRestoreResponse(backupRestore))
+
+		HttpApiClient client = new HttpApiClient()
+
+		try {
+			def cloudId = backupResult.zoneId ?: backupResult.backup?.zoneId
+			if(!cloudId) {
+				rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				rtn.data.backupRestore.errorMessage = "Associated cloud not found"
+				rtn.success = false
+				rtn.data.updates = true
+				return rtn
+			}
+
+			def serverId = backupResult.serverId ?: backupResult.backup?.computeServerId
+			if(!serverId) {
+				rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				rtn.data.backupRestore.errorMessage = "Associated server not found"
+				rtn.success = false
+				rtn.data.updates = true
+				return rtn
+			}
+
+			def cloud = context.services.cloud.get(cloudId)
+
+			// now that we've got our cloud, set the proxy
+			client.networkProxy = cloud.apiProxy
+
+			def server = context.services.computeServer.get(serverId)
+			def resp = NutanixPrismElementApiService.restoreSnapshot(client, [zone: cloud, vmId: server.externalId, snapshotId:backupRestore.externalId])
+			if (!resp.success) {
+				rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				rtn.data.backupRestore.errorMessage = resp?.msg
+				rtn.success = false
+				rtn.data.updates = true
+			}
+
+			rtn.data.backupRestore.externalId = resp.results?.taskUuid
+		} finally {
+			client.shutdownClient()
+		}
+
+		return rtn
 	}
 
 	/**
@@ -82,6 +129,59 @@ class NutanixPrismElementBackupRestoreProvider implements BackupRestoreProvider 
 	 */
 	@Override
 	ServiceResponse<BackupRestoreResponse> refreshBackupRestoreResult(BackupRestore backupRestore, BackupResult backupResult) {
-		return ServiceResponse.success()
+		log.debug("Refreshing backup restore with result {}", backupResult)
+		ServiceResponse<BackupRestoreResponse> rtn = new ServiceResponse<>(false, null, null, new BackupRestoreResponse(backupRestore))
+
+		HttpApiClient client = new HttpApiClient()
+
+		try {
+			def cloudId = backupResult.zoneId ?: backupResult.backup?.zoneId
+			if(!cloudId) {
+				rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				rtn.data.backupRestore.errorMessage = "Associated cloud not found"
+				rtn.success = false
+				rtn.data.updates = true
+				return rtn
+			}
+
+			def serverId = backupResult.serverId ?: backupResult.backup?.computeServerId
+			if(!serverId) {
+				rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				rtn.data.backupRestore.errorMessage = "Associated server not found"
+				rtn.success = false
+				rtn.data.updates = true
+				return rtn
+			}
+
+			def cloud = context.services.cloud.get(cloudId)
+
+			// now that we've got our cloud, set the proxy
+			client.networkProxy = cloud.apiProxy
+
+			def taskResults = NutanixPrismElementApiService.getTask(client, [zone: cloud], backupRestore.externalId)
+			if(taskResults.success == true && taskResults.results.percentage_complete == 100) {
+				def results = taskResults.results
+				if(results.progress_status == 'Succeeded') {
+					rtn.success = true
+					rtn.data.backupRestore.status = BackupResult.Status.SUCCEEDED
+				} else if(results.progress_status == 'Failure' || results.progress_status == 'Failed') {
+					rtn.data.backupRestore.status = BackupResult.Status.FAILED
+				}
+				rtn.data.backupRestore.endDate = results.completeTime
+
+
+				def startTime = results.start_time_usecs
+				def endTime = results.complete_time_usecs
+				if(startTime && endTime) {
+					rtn.data.backupRestore.duration = (endTime - startTime) / 1000
+				}
+
+				rtn.data.updates = true
+			}
+		} finally {
+			client.shutdownClient()
+		}
+
+		return rtn
 	}
 }
